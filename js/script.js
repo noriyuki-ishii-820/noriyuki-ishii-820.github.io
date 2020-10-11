d8f753540d67756a08c9a99586133760ba2575b9
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-10-11 22:58:27 +1100

    polished with js and css

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$("#secret").on("click", function () {
+  alert("How did you find me!?");
+  alert("Here is a quick joke.");
+  alert("What do you eat when you need to cool down?");
+  alert("...");
+  alert("Chili");
+});
