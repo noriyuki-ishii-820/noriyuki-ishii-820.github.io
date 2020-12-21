eff9645fdc6561cec8c4a23b18fa631ad6fd3cad
Nori Ishii <noriishii@Noris-MacBook-Pro.local>
2020-12-21 22:21:11 +1100

    mixitup added

--- old/js/script.js
+++
@@ -1,7 +0,0 @@
-$("#secret").on("click", function () {
-  alert("How did you find me!?");
-  alert("Here is a quick joke.");
-  alert("What do you eat when you need to cool down?");
-  alert("...");
-  alert("Chili");
-});
